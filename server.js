61186fa542492a81cde4462c09996dfc4b177807
chanki <alscksrl2010@gmail.com>
2020-08-19 15:17:00 +0900

    Add cron job of whoCrawler
    server.js에 WHO 크롤러를 돌리는 스케쥴 추가

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
-const _ = require('lodash');
-require('dotenv').config();
-require('./utils.js')();
-//require('./crawler.js')();
-const GisaidCrawler = require('./GisaidCrawler');
+require("dotenv").config();
+require("./utils.js")();
 
+const GisaidCrawler = require("./GisaidCrawler");
+const WhoCrawler = require("./Crawler/WhoCrawler");
 
-const schedule = require('node-schedule');
+const schedule = require("node-schedule");
 const GISAID_CRON = process.env.CRWALER_GISALD_CRON;
 const WHO_CRON = process.env.CRWALER_WHO_CRON;
 
-
-
 (async () => {
-    const crawler = new GisaidCrawler();
+    const gisaidCrawler = new GisaidCrawler();
+    const whoCrawler = new WhoCrawler();
+
     schedule.scheduleJob(GISAID_CRON, async () => {
-        console.log('running gisaid crawler')
-        await crawler.crawlGisaid();
-    })
+        console.log("running gisaid crawler");
+        //kawait gisaidCrawler.crawlGisaid();
+    });
+
+    schedule.scheduleJob(WHO_CRON, async () => {
+        console.log("running who crawler");
+        await whoCrawler.crawlWho();
+    });
 })();
 
 return 0;
